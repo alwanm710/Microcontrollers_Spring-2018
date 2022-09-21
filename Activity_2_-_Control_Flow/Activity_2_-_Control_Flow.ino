773625962d2bd1af04ad2f5675da776eb6342e44
Mohammad Alwan <99037011+alwanm710@users.noreply.github.com>
2022-09-21 11:53:57 -0500

    Add files via upload

---
+++ Activity_2_-_Control_Flow/Activity_2_-_Control_Flow.ino
@@ -0,0 +1,38 @@
+/*
+ * Jeff, Mohammad
+ * Activity 2
+ * INnputs from a DIP switch change the color of an RGB  LED
+ * OUtputs: D7-D5
+ * Inputs: D13-D12 (A), D10-D8 (B)
+ */
+
+void setup() {
+
+  DDRD |= 0b11100000; //outputs to rbg led
+
+}
+
+void loop() {
+
+  unsigned char a, b, c;
+  a = PINB & 0b00000111; //inputs from a
+  b = PINB & 0b00111000; //inputs from b
+  c = ( b >> 3 ); //bitshift b to isolate those inputs
+
+  if ( a > c )
+  {
+    PORTD |= 0b10000000; //if a > c light is red
+    }
+  if ( a < c )
+  {
+    PORTD |= 0b01000000; // if a < c light is green
+  }
+  if ( a == c )
+  {
+    PORTD |= 0b00100000; //if a = c light is blue
+    }
+
+  PORTD &= 0x00; // clears all outputs so that only one color is lit at a time
+
+
+}
